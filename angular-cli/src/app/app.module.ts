917ef67b1bea48413257a924c91f496a8bdbfd89
Cleicy Guião <cle.mad@hotmail.com>
2018-04-19 17:06:20 -0300

    Criação do Modulo Noticia

--- angular-cli/src/app/app.module.ts
+++ angular-cli/src/app/app.module.ts
@@ -1,31 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
+import { NoticiaModule } from './noticia/noticia.module';
 
 import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
 import { RodapePComponent } from './rodape-p/rodape-p.component';
-import { NoticiaComponent } from './noticia/noticia.component';
 import { AlertaComponent } from './alerta/alerta.component';
-import { PrimeiroexemploComponent } from './primeiroexemplo/primeiroexemplo.component';
-import { SegundoexemploComponent } from './segundoexemplo/segundoexemplo.component';
-import { TerceiroexemploComponent } from './terceiroexemplo/terceiroexemplo.component';
 
 @NgModule({
   declarations: [
     TopoComponent,
     AppComponent,
-    RodapePComponent,
-    NoticiaComponent,
     AlertaComponent,
-    PrimeiroexemploComponent,
-    SegundoexemploComponent,
-    TerceiroexemploComponent
+    RodapePComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    NoticiaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular-cli/src/app/noticia/noticia.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'
+
+import { NoticiaComponent } from './noticia.component';
+import { PrimeiroexemploComponent } from '../primeiroexemplo/primeiroexemplo.component';
+import { SegundoexemploComponent } from '../segundoexemplo/segundoexemplo.component';
+import { TerceiroexemploComponent } from '../terceiroexemplo/terceiroexemplo.component';
+
+
+@NgModule({
+  imports: [
+    FormsModule,
+    CommonModule
+  ],
+  declarations: [
+    NoticiaComponent,
+    PrimeiroexemploComponent,
+    SegundoexemploComponent,
+    TerceiroexemploComponent
+  ],
+exports: [
+  NoticiaComponent,
+]
+}
+})
+export class NoticiaModule { }
+
+
